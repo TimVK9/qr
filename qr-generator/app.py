cbb751674a2f1483cf86ec037f4700c5cfd2098e
kalin <kalinin.timofe@mail.ru>
2026-02-09 00:36:23 +0700

    feat: add input clearing on make another, fix: set SECRET_KEY from env, config: add .env to gitignore

--- qr-generator/app.py
+++ qr-generator/app.py
@@ -290,7 +290,7 @@ def sitemap():
     return """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    <url>
-      <loc>https://your-domain.com/</loc>
+      <loc>https://qrgenerator-9iyo.onrender.com/</loc>
       <lastmod>2024-01-01</lastmod>
       <changefreq>weekly</changefreq>
       <priority>1.0</priority>
